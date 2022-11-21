a2f8331cfbae9faeecf2b5ab32475c1a672817bd
smurfedGitHubIO <clydefrongoso@gmail.com>
2022-11-21 22:10:30 +0800

    10226 Hardwood Species

--- Nov 21 Mock/10226 Hardwood Species.cpp
+++ Nov 21 Mock/10226 Hardwood Species.cpp
@@ -1,22 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int ll;
+
 int main(){
   ll t;
   cin >> t;
-  while(t--){
-    char s[5000];
+  vector<string> vs[t];
+  for(ll i=0; i<t; i++){
+    if(i == 0){
+      string s;
+      ll cnt = 0;
+      ll q = 0;
+      while(getline(cin, s)){
+        if(s[0] == '\0'){
+          if(q++ <= 1){
+            continue;
+          } else {
+            break;
+          }
+        } else {
+          ll n = s.length();
+          if(s[n-1] == '\n')
+            s[n-1] = '\0';
+          vs[i].emplace_back(s);
+        }
+      }
+    } else {
+      char s[5000];
+      map<string, double> msl;
+      ll cnt = 0;
+      while(fgets(s, 5000, stdin)){
+        if(s[0] == '\n' or s[0] == '\0'){
+          break;
+        } else {
+          ll n = strlen(s);
+          if(s[n-1] == '\n')
+            s[n-1] = '\0';
+          vs[i].emplace_back(s);
+        }
+      }
+    }
+  }
+  for(ll i=0; i<t; i++){
     map<string, ll> msl;
-    ll cnt = 0;
-    while(scanf("%[^\n]%*c", s)){
-      if(s == "") break;
-      msl[s] += 1;
-      cnt += 1;
+    for(ll j=0; j<(ll)vs[i].size(); j++){
+      msl[vs[i][j]] += 1;
     }
     for(auto x : msl){
-      float pt = (x.second*100)/cnt;
-      cout << fixed << setprecision(5) << x.first << " " << pt << "\n";
+      double sz = vs[i].size();
+      double pt = (x.second*100.00)/sz;
+      cout << fixed << setprecision(4) << x.first << " " << pt << '\n';
     }
+    if(i != t-1)
+      cout << "\n";
   }
   return 0;
 }
