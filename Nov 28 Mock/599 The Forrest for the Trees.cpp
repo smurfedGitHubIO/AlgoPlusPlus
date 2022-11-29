457b0c86955ef09c1b1e4bcff8ada9ef3edcd6e4
smurfedGitHubIO <clydefrongoso@gmail.com>
2022-11-29 20:08:35 +0800

    Nov 28 Mock

---
+++ Nov 28 Mock/599 The Forrest for the Trees.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+  ll t;
+  cin >> t;
+  map<char, ll> mcl;
+  for(ll j=0; j<26; j++){
+    mcl[char('A'+j)] = j;
+  }
+  while(t--){
+    string s, p;
+    vector<string> vs;
+    vector<ll> v[26];
+    while(1){
+      cin >> s;
+      if(s[0] == '*') break;
+      vs.emplace_back(s);
+    }
+    cin >> s;
+    for(ll i=0; i<vs.size(); i++){
+      ll p = mcl[vs[i][1]], q = mcl[vs[i][3]];
+      v[p].emplace_back(q);
+      v[q].emplace_back(p);
+    }
+    vector<bool> vis(26, false);
+    ll trees = 0, acorns = 0;
+    for(ll i=0; i<s.length(); i+=2){
+      ll j = mcl[s[i]];
+      if(!vis[j]){
+        if(v[j].size() == 0){
+          acorns++;
+        }
+        else {
+          trees ++;
+          stack<ll> stl;
+          stl.push(j);
+          vis[j] = 1;
+          while(!stl.empty()){
+            ll tp = stl.top();
+            stl.pop();
+            for(auto x : v[tp]){
+              if(!vis[x]){
+                vis[x] = 1;
+                stl.push(x);
+              }
+            }
+          }
+        }
+      }
+    }
+    cout << "There are " << trees << " tree(s) and " << acorns << " acorn(s).\n";
+  }
+  return 0;
+}
